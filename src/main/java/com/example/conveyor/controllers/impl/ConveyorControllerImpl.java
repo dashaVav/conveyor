f506110f03ac8e8718d860795598351805a10b1f
DashaVav <dashavavilova111002@gmail.com>
2024-07-11 17:14:10 +0300

    remove prescoring

--- src/main/java/com/example/conveyor/controllers/impl/ConveyorControllerImpl.java
+++ src/main/java/com/example/conveyor/controllers/impl/ConveyorControllerImpl.java
@@ -5,7 +5,6 @@ import com.example.conveyor.dtos.CreditDTO;
 import com.example.conveyor.dtos.LoanApplicationRequestDTO;
 import com.example.conveyor.dtos.LoanOfferDTO;
 import com.example.conveyor.dtos.ScoringDataDTO;
-import com.example.conveyor.services.PrescoringService;
 import com.example.conveyor.services.ScoringService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +15,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class ConveyorControllerImpl implements ConveyorController {
-    private final PrescoringService prescoringService;
     private final ScoringService scoringService;
 
     public ResponseEntity<List<LoanOfferDTO>> offers(LoanApplicationRequestDTO loanApplicationRequest) {
-        prescoringService.validationOfLoanApplicationRequest(loanApplicationRequest);
         return ResponseEntity.ok(scoringService.createLoanOffers(loanApplicationRequest));
     }
 
     public ResponseEntity<CreditDTO> calculation(ScoringDataDTO scoringData) {
-        prescoringService.validationOfScoringData(scoringData);
         return ResponseEntity.ok(scoringService.createPersonalCreditOffer(scoringData));
     }
 }
